fbb9f5ae88ef002b4bd4ce7e3e6df3a1d2e89fe5
N <arzumanannarine1@gmail.com>
2026-02-09 22:48:38 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,12 @@
+"""
+WSGI файл для запуска на Render с gunicorn
+"""
+
 from app import app
 
+# Gunicorn ищет переменную 'application' или 'app'
+# У нас есть 'app', так что всё ок
+
 if __name__ == "__main__":
-    app.run()
+    # Локальный запуск для тестов
+    app.run(debug=True)
